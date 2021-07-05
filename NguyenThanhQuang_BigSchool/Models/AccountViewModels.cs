d4462313bd2261eb8caa0723af5b3b02f2fd4d8e
THANH QUANG <THANH QUANG@DESKTOP-0G74NBM>
2021-07-05 10:52:26 +0700

    trang35

--- NguyenThanhQuang_BigSchool/Models/AccountViewModels.cs
+++ NguyenThanhQuang_BigSchool/Models/AccountViewModels.cs
@@ -79,6 +79,9 @@ namespace NguyenThanhQuang_BigSchool.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
     }
 
     public class ResetPasswordViewModel
